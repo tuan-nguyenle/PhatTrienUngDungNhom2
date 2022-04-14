2bd7495bd67a04ef0ce19772628260d25cc6b711
unknown <levanluong6a8@gmail.com>
2022-04-14 19:32:26 +0700

    luong ne hehe

---
+++ View/header.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar--header navbar-inverse fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <span class="text-header"><i class="fas fa-school"></i> Tên Trường : THCS Võ Thị Sáu&emsp;
+                    <i class="fa fa-envelope"></i>E-mail :<a href="mailto:19503891.tuan@student.iuh.edu.vn">19503891.tuan@student.iuh.com.vn</a></span>
+            </div>
+            <div class="dropdown"><a role="button" class="nav-link dropdown-toggle text-light" data-toggle="dropdown">
+                    <i class="fa fa-user"></i> Lê Nguyễn Tuân</a>
+                <div class="dropdown-menu text-center menu--user">
+                    <a href="admin.php" class="dropdown-item"><i class="fa fa-house-user" style="color: #95b87b;"></i>  <span>Nhà</span></a>
+                    <a href="myinfo.php" class="dropdown-item"><i class="fa fa-user-circle" style="color: #95b87b;"></i>  <span>Hồ Sơ</span></a>
+                    <a href="frontend/logout.php" class="dropdown-item"><i class="fas fa-sign-out-alt" style="color: #95b87b;"></i>  <span>Đăng Xuất</span></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="./Image/lms-learning-management-system-concept-with-big-vector-27335395.jpg">
+    <link rel="stylesheet" href="./bootrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootrap/fontawesome-free-5.15.3-web/css/all.css">
+    <link rel="stylesheet" href="./bootrap/fontawesome-free-5.15.3-web/css/fontawesome.css">
+    <link rel="stylesheet" href="./bootrap/fontawesome-free-5.15.3-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="./CSS/header_footer.css">
+    <script src="./bootrap/fontawesome-free-5.15.3-web/js/all.js"></script>
+    <script src="./bootrap/JS/jquery.min.js"></script>
+    <script src="./bootrap/JS/popper.min.js"></script>
+    <script src="./bootrap/JS/bootstrap.min.js"></script>
+    <title>Hệ thống học tập trực tuyến</title>
+</head>
+
+<body>
+    <?php include_once './View/header.php'; ?>
+    <div class="blabla">
+    
+    </div>
+    <?php include_once './View/footer.php'; ?>
+</body>
+
+</html>
