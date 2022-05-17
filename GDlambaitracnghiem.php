c17ae896e649b3849b2a03fe8c6edd7e0107650d
Ngọc Trâm <77617498+lethingoctram@users.noreply.github.com>
2022-05-17 20:49:18 +0700

    Add files via upload

---
+++ GDlambaitracnghiem.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="../PhatTrienUngDungNhom2-main/Image/lms-learning-management-system-concept-with-big-vector-27335395.jpg">
+    <!-- Latest compiled JavaScript -->
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/bootrap/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/bootrap/fontawesome-free-5.15.3-web/css/all.css">
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/bootrap/fontawesome-free-5.15.3-web/css/fontawesome.css">
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/bootrap/fontawesome-free-5.15.3-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/CSS/header_footer.css">
+    <link rel="stylesheet" href="../PhatTrienUngDungNhom2-main/CSS/style.css">
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/fontawesome-free-5.15.3-web/js/all.js"></script>
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/JS/jquery.min.js"></script>
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/JS/popper.min.js"></script>
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/JS/bootstrap.min.js"></script>
+    <script src="../PhatTrienUngDungNhom2-main/bootrap/JS/ckeditor.js"></script>
+    <title>Hệ thống học tập trực tuyến</title>
+    </head>
+
+<body>
+    <header>
+    <nav class="navbar navbar--header navbar-inverse fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <span class="text-header"><i class="fas fa-school"></i> Tên Trường :
+                    Võ Thị Sáu                    <!-- THCS Võ Thị Sáu -->
+                    &emsp;<i class="fa fa-envelope"></i>E-mail :<a href="mailto:19503891.tuan@student.iuh.edu.vn">19503891.tuan@student.iuh.com.vn</a>
+                </span>
+            </div>
+                                        <div class="dropdown"><a role="button" class="nav-link dropdown-toggle text-light" data-toggle="dropdown" id="dropdownMenuButton">
+                                    <img style="max-width: 35px;max-height: 35px;" src='../PhatTrienUngDungNhom2-main/Image/df_user.png'></img>&emsp;Nguyễn Trần Hoàng Hà</a>
+                                <div class="dropdown-menu text-center menu--user" labelledby="dropdownMenuButton">
+                                    <a href="../PhatTrienUngDungNhom2-main/index.php" class="dropdown-item"><i class="fa fa-house-user" style="color: #95b87b;"></i> <span>Nhà</span></a>
+                                    <a href="../PhatTrienUngDungNhom2-main/?myInfo" class="dropdown-item"><i class="fa fa-user-circle" style="color: #95b87b;"></i> <span>Hồ Sơ</span></a>
+                                    <a href="../PhatTrienUngDungNhom2-main/?logout" class="dropdown-item"><i class="fas fa-sign-out-alt" style="color: #95b87b;"></i> <span>Đăng Xuất</span></a>
+                                </div>
+                            </div>
+                                </div>
+    </nav>
+</header>    <div id="navbar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <!--               <a class="navbar-brand" href="#">Navbar</a>
+--> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse navbar-center" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="../PhatTrienUngDungNhom2-main/index.php">Trang chủ</a>
+          <a class='nav-link active' href='../PhatTrienUngDungNhom2-main/?xemThongTinMonHoc'>Xem Thông Tin bài học</a>        </div>
+      </div>
+    </div>
+  </nav>
+</div>
+<div class="container">
+    <div class="subject" >
+        <p class="h3" style="color:#C00; text-align: center">Đề Kiểm Tra Trắc Nghiệm môn Toán</p>
+        <p>&nbsp;</p>
+        <div class="sider-bar">
+            <aside style="display: block; width: 20%; background-color:#ADD6AB; margin-left: 10px; float: right;"> 
+            	<div class="head-side" style="padding-left: 20px; padding-top:30px;">
+                    <p>Thời gian làm bài còn lại</p>
+                    <p style="padding-left:80px">07:52 s</p>
+                    <p>Số lượng câu hỏi</p>
+                </div>
+           		<div class="span" align="center"> 
+                    <span class="1" style="width:30px; height:10px; padding:2px; background-color:#FFF">1</span>
+                    <span class="2" style="width:30px; height:10px; padding:2px; background-color:#FFF">2</span>
+                    <span class="3" style="width:30px; height:10px; padding:2px; background-color:#FFF">3</span>
+                    <span class="4" style="width:30px; height:10px; padding:2px; background-color:#FFF">4</span>
+                    <span class="5" style="width:30px; height:10px; padding:2px; background-color:#FFF">5</span>
+                    <br/>
+                    <br/>
+                    <span class="6" style="width:30px; height:10px; padding:2px; background-color:#FFF">6</span>
+                    <span class="7" style="width:30px; height:10px; padding:2px; background-color:#FFF">7</span>
+                    <span class="8" style="width:30px; height:10px; padding:2px; background-color:#FFF">8</span>
+                    <span class="9" style="width:30px; height:10px; padding:2px; background-color:#FFF">9</span>
+                    <span class="10" style="width:30px; height:10px; padding:2px; background-color:#FFF">10</span>
+                    <br/>
+                    <br/>
+                    <input type="button" data-toggle="modal" data-target="#nopbai" name="nopbai" style=" background-color:#CCC; float:center; padding:5px;" value="Kết thúc" >
+                </div>
+            	<div class="modal" id="nopbai">
+               		<div class="modal-dialog-centered" style="width:300px; float:center; align:center; margin-left:600px">
+                		<div class="modal-content" >
+                        	<div class="modal-body">
+                            <p>Bạn chắc chắn nộp bài</p>
+                        	</div>
+                        	<div class="modal-footer">
+                            	
+                            	<input name="xacnhan" id="xn" type="submit" style=" background-color:#096; float:center; padding:2px;" value="Xác nhận" >
+                           		<button type="button" class="btn btn-default" data-dismiss="modal" style=" background-color:#096; float:center; padding:2px">Trở lại</button>
+                        	</div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </div>
+   <div class="cau1" style="display: block; background-color:#CCC; width: 1000px; padding-left:20px">
+       <p style=" padding-top:2px"><h5>Câu 1</h5></p>
+       <p>Xét tập hợp N, trong các số sau số nào là bội của 14 ?</p>
+       <form class="cau1" style=" padding-left:50px">
+       		<input type="radio" name="dap_an" id="A" value="A" />
+			<label for="A">A. 48 </label><br/>
+            <input type="radio" name="dap_an" id="B" value="B" />
+			<label for="B">B. 28 </label><br/>
+            <input type="radio" name="dap_an" id="C" value="C" />
+			<label for="C">C. 36 </label><br/>
+            <input type="radio" name="dap_an" id="D" value="D" />
+			<label for="D">D. 7 </label><br/>
+       </form>
+   </div>
+   <p>&nbsp;</p>
+   <div class="cau2" style="display: block; background-color:#CCC; width: 1000px; padding-left:20px">
+       <p style=" padding-top:2px"><h5>Câu 2</h5></p>
+       <p>Phân tích 24 ra thừa số nguyên tố – Cách dùng đúng là:</p>
+       <form class="cau1" style=" padding-left:50px">
+       		<input type="radio" name="dap_an" id="A" value="A" />
+			<label for="A">A. 24 = 4 . 6 = 2^2. 6 </label><br/>
+            <input type="radio" name="dap_an" id="B" value="B" />
+			<label for="B">B. 24 = 2^3. 3  </label><br/>
+            <input type="radio" name="dap_an" id="C" value="C" />
+			<label for="C">C. 24 = 24 . 1 </label><br/>
+            <input type="radio" name="dap_an" id="D" value="D" />
+			<label for="D">D. 24 = 2 x 12 </label><br/>
+       </form>
+
+   </div>
+   <p>&nbsp;</p>
+   <div class="row">
+        <div class="col-6">
+            <input type="button" data-toggle="modal" data-target="#nopbai" name="nopbai" style=" background-color:#096; float: right; padding:5px;" value="Nộp bài">
+            <div class="modal" id="nopbai">
+                <div class="modal-dialog-centered" style="width:300px; float:center; align:center; margin-left:600px">
+                <div class="modal-content" >
+                        <div class="modal-body">
+                            <p>Bạn chắc chắn nộp bài?</p>
+                        </div>
+                        <div class="modal-footer">
+                         	<input name="xacnhan" id="xn" type="submit" style=" background-color:#096; float:right; padding:2px;" value="Xác nhận nộp bài" >
+                            <button type="button" class="btn btn-default" data-dismiss="modal" style=" background-color:#096; float:right; padding:2px">Trở lại</button>
+                        
+                        </div>
+                    
+                    </div>
+                </div>
+             </div>
+           </div>   
+    <p>&nbsp;</p>
+   	<p>&nbsp;</p>
+   	<p>&nbsp;</p>
+	</div>
+</div>
+<footer class="pt-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <img src="//lms.iuh.edu.vn/pluginfile.php/1/theme_academi/logo/1641353478/Logo_IUH.png" width="50%">
+                <p>Chào mừng các bạn đến với Hệ thống học tập trực tuyến của nhóm 2 thực hiện.</p>
+            </div>
+            <div class="col-md-3">
+                <div class="foot-links">
+                    <!--<h2>Liên kết</h2>-->
+                    <h2>Liên kết</h2>
+                    <ul>
+                        <li><a href="http://iuh.edu.vn/" target="_blank">Website Nhà Trường</a></li>
+                        <li><a href="https://csm.iuh.edu.vn/" target="_blank">Website Trung tâm QTHT</a></li>
+                        <li><a href="https://sv.iuh.edu.vn/" target="_blank">Cổng Thông Tin Sinh Viên</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="contact-info">
+                    <h2 class="nopadding">Liên hệ</h2>
+
+                    <p>Hệ thống học tập trực tuyến<br>
+                        <i class="fa fa-phone-square"></i> ĐT : 0869.236 512<br>
+                        <i class="fa fa-envelope"></i> E-mail : <a class="mail-link" href="mailto:19503891.tuan@student.iuh.edu.vn">19503891.tuan@student.iuh.edu.vn</a><br>
+                    </p>
+
+                </div>
+                <div class="social-media">
+                    <h6>Follow us</h6>
+                    <ul>
+                        <li class="smedia-01"><a href="https://www.facebook.com/csm.iuh.edu.vn"><i class="fab fa-facebook-square" style="color: white;"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>  
+</body>
+
+</html>
